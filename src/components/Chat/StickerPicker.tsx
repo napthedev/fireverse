d0acf7a19e98ea1169248f3e7fe4d3449332e0ec
napthedev <phongna.dev@gmail.com>
2022-02-02 14:36:11 +0700

    Feat: Add recent stickers to localstorage

--- src/components/Chat/StickerPicker.tsx
+++ src/components/Chat/StickerPicker.tsx
@@ -1,5 +1,6 @@
+import { FC, useState } from "react";
+
 import ClickAwayListener from "../ClickAwayListener";
-import { FC } from "react";
 import { STICKERS_URL } from "../../shared/constants";
 import Spin from "react-cssfx-loading/src/Spin";
 import SpriteRenderer from "../SpriteRenderer";
@@ -11,10 +12,31 @@ interface StickerPickerOpened {
   onSelect: (value: string) => void;
 }
 
+const getRecentStickers = () => {
+  const existing = localStorage.getItem("fireverse-recent-stickers") || "[]";
+  try {
+    const parsed = JSON.parse(existing);
+    if (Array.isArray(parsed)) return parsed;
+    return [];
+  } catch (error) {
+    return [];
+  }
+};
+
 const StickerPicker: FC<StickerPickerOpened> = ({ setIsOpened, onSelect }) => {
   const { data, loading, error } = useFetch<StickerCollections>("sticker", () =>
     fetch(STICKERS_URL).then((res) => res.json())
   );
+
+  const [recentStickers, setRecentStickers] = useState(getRecentStickers());
+
+  const addRecentSticker = (url: string) => {
+    const added = [...new Set([url, ...recentStickers])];
+
+    localStorage.setItem("fireverse-recent-stickers", JSON.stringify(added));
+
+    setRecentStickers(added);
+  };
 
   return (
     <ClickAwayListener onClickAway={() => setIsOpened(false)}>
@@ -29,15 +51,40 @@ const StickerPicker: FC<StickerPickerOpened> = ({ setIsOpened, onSelect }) => {
             </div>
           ) : (
             <div className="w-full h-full flex flex-col">
-              <div className="flex-grow overflow-y-auto p-3">
+              <div className="flex-grow overflow-y-auto p-3 pt-1">
+                {recentStickers.length > 0 && (
+                  <>
+                    <h1 className="mt-2" id={`sticker-recent`}>
+                      Recent stickers
+                    </h1>
+                    <div className="w-full grid grid-cols-5 justify-between">
+                      {recentStickers.map((url) => (
+                        <SpriteRenderer
+                          onClick={() => {
+                            onSelect(url);
+                            addRecentSticker(url);
+                            setIsOpened(false);
+                          }}
+                          className="hover:bg-dark-lighten cursor-pointer"
+                          src={url}
+                          runOnHover
+                        />
+                      ))}
+                    </div>
+                  </>
+                )}
+
                 {data?.map((collection) => (
                   <>
-                    <h1 id={`sticker-${collection.id}`}>{collection.name}</h1>
+                    <h1 className="mt-2" id={`sticker-${collection.id}`}>
+                      {collection.name}
+                    </h1>
                     <div className="w-full grid grid-cols-5 justify-between">
                       {collection.stickers.map((sticker) => (
                         <SpriteRenderer
                           onClick={() => {
                             onSelect(sticker.spriteURL);
+                            addRecentSticker(sticker.spriteURL);
                             setIsOpened(false);
                           }}
                           className="hover:bg-dark-lighten cursor-pointer"
@@ -51,13 +98,25 @@ const StickerPicker: FC<StickerPickerOpened> = ({ setIsOpened, onSelect }) => {
               </div>
 
               <div className="flex-shrink-0 h-18 flex gap-2 w-full overflow-x-auto border-t border-t-dark-lighten p-2">
+                {recentStickers.length > 0 && (
+                  <button
+                    onClick={() =>
+                      document
+                        .querySelector(`#sticker-recent`)
+                        ?.scrollIntoView()
+                    }
+                    className="w-9 h-9 flex items-center"
+                  >
+                    <i className="bx bx-time text-2xl"></i>
+                  </button>
+                )}
                 {data?.map((collection) => (
                   <img
-                    onClick={() => {
+                    onClick={() =>
                       document
                         .querySelector(`#sticker-${collection.id}`)
-                        ?.scrollIntoView();
-                    }}
+                        ?.scrollIntoView()
+                    }
                     className="w-9 h-9 object-cover cursor-pointer"
                     src={collection.icon}
                     alt=""
